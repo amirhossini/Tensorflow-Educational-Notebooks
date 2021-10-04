1bf192e19369e7571da611d995ea23c29e814989
amirhossini <amir.hossini@queensu.ca>
2021-10-03 20:04:14 -0600

    Complete!

--- 00-Simple_DNN_and_CNN/tf_core_image_classification.py
+++ 00-Simple_DNN_and_CNN/tf_core_image_classification.py
@@ -2,10 +2,11 @@
 Original Version from tensorflow.org
 Available @ https://www.tensorflow.org/tutorials/images/classification
 
-Recreated in tensorflow 2.5 by Amir Hossini:
+Recreation in Tensorflow 2.5 (& Python 3.8) by Amir Hossini:
  - Addition of ImageDataGenerator
  - Split Function
- - Custom Callbacks: min accuracy
+ - Loss Function: categorical_cross_entropy (instead of sparse_categorical_cross_entropy)
+ - Custom Callbacks: min required accuracy
 """
 ## Libraries
 import matplotlib.pyplot as plt
@@ -26,10 +27,33 @@ import pathlib
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 ## Folder setup
-data_folder=f"{os.getcwd()}/Datasets/flower_photos"
+data_folder=f"{os.getcwd()}/datasets/flower_photos"
+
+## Parameters
+seed             = 42
+
+split_size       = 0.8
+batch_size_train = 32
+batch_size_valid = 16
+img_height       = 180
+img_width        = 180
+
+max_n_epochs     = 20
+min_accuracy     = 0.6
+
+## Set random seed
+tf.random.set_seed=seed
 
 ## Functions
-def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+class Callback_set(tf.keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('accuracy') is not None and logs.get('val_accuracy') > 0.7):
+            print(f"\nReached 70% validation accuracy so cancelling training!")
+            self.model.stop_training = True
+
+def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE, seed=42):
+  random.seed(seed)
   src_list = os.listdir(SOURCE)
   src_shuf = random.sample(src_list, len(src_list))
   src_clnn = []
@@ -54,10 +78,9 @@ def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
     copyfile(fsrc, fdes)
   return
 
-def model_fit(num_classes, train_ds,val_ds,epochs=10, callbacks=None):
+def model_compile(num_classes):
     model = Sequential([
-        layers.InputLayer(input_shape=(img_height, img_width, 3)),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(img_height, img_width, 3)),
         layers.MaxPooling2D(),
         layers.Conv2D(32, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
@@ -65,21 +88,19 @@ def model_fit(num_classes, train_ds,val_ds,epochs=10, callbacks=None):
         layers.MaxPooling2D(),
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
-        layers.Dense(num_classes)
+        layers.Dense(num_classes, activation=tf.keras.activations.softmax)
     ])
     print(model.summary())
 
     model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  loss='categorical_crossentropy',
                   metrics=['accuracy'])
-
-    # history = model.fit(
-    #     train_ds,
-    #     validation_data=val_ds,
-    #     epochs=epochs,
-    #     callbacks=callbacks
-    # )
-    return
+    """
+    Attention must be paid to application of sparse_categorical vs categorical_cross_entropy for loss functions 
+    when working with image generators:
+    https://stackoverflow.com/questions/49161174/tensorflow-logits-and-labels-must-have-the-same-first-dimension
+    """
+    return model
 
 ## Load Data
 
@@ -88,7 +109,7 @@ data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=Tr
 data_dir = pathlib.Path(data_dir)
 shutil.copytree(data_dir,data_folder,dirs_exist_ok=True)
 
-print('data directory: ',format(data_dir))
+print('data directory: ',format(data_folder))
 
 """
 Folder structure
@@ -111,11 +132,6 @@ PIL.Image.open(os.path.join(os.path.join(data_folder,'roses'),str(roses[0])))#.s
 PIL.Image.open(os.path.join(os.path.join(data_folder,'tulips'),str(tulips[0])))#.show()
 
 ## Train-test split
-split_size = 0.8
-batch_size = 32
-img_height = 180
-img_width = 180
-
 shutil.rmtree(f"{os.getcwd()}/tmp", ignore_errors=True)
 train_dir      = os.path.join(f"{os.getcwd()}/tmp",'train')
 validation_dir = os.path.join(f"{os.getcwd()}/tmp",'validation')
@@ -127,7 +143,7 @@ for catg in categories:
   os.makedirs(train_dir_cat, exist_ok=True)
   valid_dir_cat = os.path.join(validation_dir,catg)
   os.makedirs(valid_dir_cat, exist_ok=True)
-  split_data(source_cat,train_dir_cat,valid_dir_cat,split_size)
+  split_data(source_cat,train_dir_cat,valid_dir_cat,split_size,seed)
 
 nfl_train = sum([len(f) for r,d,f in os.walk(train_dir)])
 nfl_valid = sum([len(f) for r,d,f in os.walk(validation_dir)])
@@ -136,14 +152,45 @@ dgen_train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255.)
 dgen_valid = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255.)
 
 train_gen = dgen_train.flow_from_directory(train_dir,
-                                           batch_size=batch_size,
+                                           batch_size=batch_size_train,
                                            class_mode='categorical',
                                            target_size=(img_height,img_width)
                                            )
 valid_gen = dgen_valid.flow_from_directory(validation_dir,
-                                           batch_size=batch_size,
+                                           batch_size=batch_size_valid,
                                            class_mode='categorical',
                                            target_size=(img_height,img_width)
                                            )
 
-history = model_fit(len(categories),train_gen,valid_gen,epochs=10)
+
+my_callbacks = Callback_set()
+model        = model_compile(len(categories))
+history      = model.fit(train_gen,
+                         steps_per_epoch  = int(nfl_train / batch_size_train),
+                         validation_data  = valid_gen,
+                         validation_steps = int(nfl_valid / batch_size_valid),
+                         epochs           = max_n_epochs,
+                         callbacks        = [my_callbacks]
+                         )
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(len(acc))
+
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
