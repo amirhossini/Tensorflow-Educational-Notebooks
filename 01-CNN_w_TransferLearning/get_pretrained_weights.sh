45903b4c8f30be1692eed85d876f3620054ae07b
amirhossini <amir.hossini@queensu.ca>
2021-10-08 11:28:11 -0600

    .sh record

---
+++ 01-CNN_w_TransferLearning/get_pretrained_weights.sh
@@ -0,0 +1,5 @@
+# Get Pre-trained Weights for Inception Model
+
+cd ./pretrained_weights
+wget https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5
+cd ..
