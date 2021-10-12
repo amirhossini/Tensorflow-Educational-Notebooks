7ace1f927e7e6b21b4c41545cf4c939d2b5b5552
amirhossini <amir.hossini@queensu.ca>
2021-10-11 22:32:20 -0600

    embedding sentiment classification initialized!

---
+++ 02-NLP_LSTM-GRU-CNN/tf_core_sentiment_classification.py
@@ -0,0 +1,116 @@
+"""
+Original Version from tensorflow.org
+Available @ https://www.tensorflow.org/text/guide/word_embeddings
+
+Recreation in Tensorflow 2.5 (& Python 3.8) by Amir Hossini:
+ -
+"""
+## Libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import io
+import re
+import random
+import shutil
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+#from tensorflow.keras.layers import TextVectorization
+
+## GPU availability
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+## Parameters
+seed             = 42
+vocab_size       = 10000
+sequence_length  = 100
+
+## Set random seed
+tf.random.set_seed=seed
+
+## Functions
+class Callback_set(tf.keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('accuracy') is not None and logs.get('val_accuracy') > 0.95):
+            print(f"\nReached 95% validation accuracy so cancelling training!")
+            self.model.stop_training = True
+
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation), '')
+
+## Folder setup & load data
+url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+
+dataset = tf.keras.utils.get_file("aclImdb_v1.tar.gz", url,
+                                  untar=True, cache_dir='./datasets',
+                                  cache_subdir='')
+
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+print(os.listdir(dataset_dir))
+
+train_dir = os.path.join(dataset_dir, 'train')
+print(os.listdir(train_dir))
+
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
+
+batch_size = 1024
+train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'datasets/aclImdb/train', batch_size=batch_size, validation_split=0.2,
+    subset='training', seed=seed)
+val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'datasets/aclImdb/train', batch_size=batch_size, validation_split=0.2,
+    subset='validation', seed=seed)
+
+## Sample reviews
+for text_batch, label_batch in train_ds.take(1):
+  for i in range(5):
+    print(label_batch[i].numpy(), text_batch.numpy()[i])
+
+## Configurate the dataset for performance
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+## Text vectorization
+vectorize_layer = tf.keras.layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=vocab_size,
+    output_mode='int',
+    output_sequence_length=sequence_length
+)
+
+text_ds = train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(text_ds)
+
+#
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+#
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+#
+# epochs_range = range(len(acc))
+#
+# plt.figure(figsize=(10, 4))
+# plt.subplot(1, 2, 1)
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
+#
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.show()
