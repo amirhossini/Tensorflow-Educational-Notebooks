f59eb1c4c3f544da32fc92c0b0d5c96725bc70ba
Amir Hossini <91625030+amirhossini@users.noreply.github.com>
2021-10-13 10:41:25 -0600

    Update tf_core_sentiment_classification+.py

--- 02-NLP_LSTM-GRU-CNN/tf_core_sentiment_classification+.py
+++ 02-NLP_LSTM-GRU-CNN/tf_core_sentiment_classification+.py
@@ -100,6 +100,7 @@ model = Sequential([
   Embedding(vocab_size, embedding_dim, name="embedding"),
   Bidirectional(LSTM(64, return_sequences=True)),
   Bidirectional(LSTM(32)),
+  Dropout(0.2),
   Dense(64, activation='relu'),
   Dense(1, activation='sigmoid')
 ])
